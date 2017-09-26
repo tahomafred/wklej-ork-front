2348e9017587889a8904d7e92fb5b301729a7e04
tahomafred <damianborowski0@hotmail.com>
2017-09-26 12:35:29 +0200

    Add register component, minor syntax fix

---
+++ src/components/Register.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Yes yes register</h2>
+    <el-row v-loading.body="loading">
+
+    </el-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Register',
+  mixins: [require('../mixins.js')],
+  data () {
+    return {
+      pasteId: "",
+      loading: false
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
