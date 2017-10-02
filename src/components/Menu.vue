a13c8dd14ace52672255ac60f4ffdeb0043cff07
tahomafred <damianborowski0@hotmail.com>
2017-10-02 16:20:19 +0200

    add register dialog and handlers for register and login

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -23,30 +23,49 @@
         <el-button type="primary" size="mini" @click="userLogout">Log Out</el-button>
       </div>
     </el-popover>
+
     <el-dialog title="Log In" :visible.sync="dialog[0]">
-      <el-form :model="form" :rules="LogInRules" inline label-position="top">
+      <el-form ref="loginForm" :model="form[0]" :rules="LogInRules" inline label-position="top">
         <el-form-item label="Login" :label-width="formLabelWidth" prop="login">
-          <el-input v-model="form.login" auto-complete="off"></el-input>
+          <el-input v-model="form[0].login" auto-complete="off"></el-input>
         </el-form-item>
         <el-form-item label="Password" :label-width="formLabelWidth" prop="password">
-          <el-input type="password" v-model="form.password" auto-complete="off"></el-input>
+          <el-input type="password" v-model="form[0].password" auto-complete="off"></el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="handleLogin">Confirm</el-button>
+        <el-button type="primary" @click="handleLogin">Sign in</el-button>
       </span>
     </el-dialog>
-    
+
+    <el-dialog title="Register" :visible.sync="dialog[1]">
+      <el-form ref="registerForm" :model="form[1]" :rules="registerRules" label-position="top">
+        <el-form-item label="Email" :label-width="formLabelWidth" prop="email">
+          <el-input v-model="form[1].email" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Login" :label-width="formLabelWidth" prop="login">
+          <el-input v-model="form[1].login" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Password" :label-width="formLabelWidth" prop="password">
+          <el-input type="password" v-model="form[1].password" auto-complete="off"></el-input>
+        </el-form-item>
+
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="handleRegister">Register</el-button>
+      </span>
+    </el-dialog>
+
 
 
     <el-menu theme="dark" class="el-menu-demo" mode="horizontal" @select="handleSelect" :router="true">
       <el-menu-item index="/home">Wklej-ork-frontend</el-menu-item>
       <el-menu-item index="/paste">Paste</el-menu-item>
       <el-menu-item index="/view">View</el-menu-item>
-      <el-menu-item v-if="user.loggedIn" v-popover:Logged v-on:click="changePopoverState(1)" ><!-- User logged in -->
+      <el-menu-item index="/home" v-if="user.loggedIn" v-popover:Logged v-on:click="changePopoverState(1)" ><!-- User logged in -->
         <i class="fa fa-user" aria-hidden="true"></i>
       </el-menu-item>
-      <el-menu-item v-else v-popover:notLogged v-on:click="changePopoverState(0)"><!-- User not logged in -->
+      <el-menu-item index="/home" v-else v-popover:notLogged v-on:click="changePopoverState(0)"><!-- User not logged in -->
         <i class="fa fa-user-o" aria-hidden="true"></i>
       </el-menu-item>
     </el-menu>
@@ -62,12 +81,34 @@ export default {
   data () {
     return {
       popover : [{state: false}, {state: false}],
-      dialog: [false],
+      dialog: [false, false],
       formLabelWidth: "120px",
-      form: {
+      form: [
+        {
+        login: '',
+        password: ''
+        },
+        {
+        email: '',
         login: '',
         password: '',
+        }],
+
+      registerRules: {
+        login: [
+          { required: true, message: 'Please input Login', trigger: 'blur'},
+          { min: 3, message: 'Length should at least 3 characters', trigger: 'blur' }
+        ],
+        password: [
+          {required: true, message: 'Please input Password', trigger: 'blur'},
+          { min: 8, message: 'Length should at least 8 characters', trigger: 'blur' }
+        ],
+        email: [
+          {required: true, message: 'Please input Email', trigger: 'blur'},
+          { type: 'email', message: 'Please input correct email address', trigger: 'blur,change' }
+        ],
       },
+
       LogInRules: {
         login: [
           { required: true, message: 'Please input Login', trigger: 'blur'},
@@ -77,7 +118,8 @@ export default {
           {required: true, message: 'Please input Password', trigger: 'blur'},
           { min: 8, message: 'Length should at least 8 characters', trigger: 'blur' }
         ]
-      }
+      },
+
     }
   },
 
@@ -92,15 +134,43 @@ export default {
     },
     closeAllPopovers: function(){
       let popoverLength = this.popover.length;
-      for(let i=0; i <= popoverLength; i++){
+      for(let i=0; i < popoverLength; i++){
         this.popover[i].state = false;
       }
     },
     handleSelect: function(key, keyPath) {
       // I really dunno what this does, but I got a warn in the console so there it is!
     },
-    userLogin: function(){ this.dialog[0] = true; this.closeAllPopovers();},
-    userRegister: function(){this.closeAllPopovers();},
+    handleLogin: function(){
+      this.$refs['loginForm'].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+          return true;
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    handleRegister: function(){
+      this.$refs['registerForm'].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+          return true;
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    userLogin: function(){
+      this.dialog[0] = true;
+      this.closeAllPopovers();
+    },
+    userRegister: function(){
+      this.dialog[1] = true;
+      this.closeAllPopovers();
+    },
     userLogout: function(){
       this.user.loggedIn = false;
       this.closeAllPopovers();
